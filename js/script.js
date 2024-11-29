bca1ef0092efab661a0fa5504c367335cdfcafa2
akmaljon <akmal@gmail.com>
2024-11-29 11:53:06 +0500

    chizish funksiyasi yakunlandi

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
 const canvas = document.querySelector('canvas');
 
-let ctx = canvas.getContext('2d');
+let ctx = canvas.getContext('2d'),
+	isDrawing = false;
 
 window.addEventListener('load', () => {
 	canvas.width = canvas.offsetWidth;
 	canvas.height = canvas.offsetHeight;
 });
 
+const startDraw = () => {
+	isDrawing = true;
+};
+
 const drawing = e => {
+	if (!isDrawing) return;
 	ctx.lineTo(e.offsetX, e.offsetY);
 	ctx.stroke();
 };
 
+const stopDraw = () => {
+	isDrawing = false;
+};
+
+canvas.addEventListener('mousedown', startDraw);
 canvas.addEventListener('mousemove', drawing);
+canvas.addEventListener('mouseup', stopDraw);
